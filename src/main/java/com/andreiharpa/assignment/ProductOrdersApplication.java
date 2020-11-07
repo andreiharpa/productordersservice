68cffcacd10c4c537a4a224a871f01a0dfacd407
Andrei Harpa <contact@andreiharpa.dev>
2020-11-07 22:40:43 +0200

    Spring boot rest setup

---
+++ src/main/java/com/andreiharpa/assignment/ProductOrdersApplication.java
@@ -0,0 +1,11 @@
+package com.andreiharpa.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOrdersApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductOrdersApplication.class, args);
+    }
+}
---
+++ src/test/java/com/andreiharpa/assignment/ProductOrdersApplicationTests.java
@@ -0,0 +1,15 @@
+package com.andreiharpa.assignment;
+
+import org.junit.runner.RunWith;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductOrdersApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
